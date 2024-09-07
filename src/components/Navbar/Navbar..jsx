307b9f418debfa15877400f392592c68982beb3e
sifat18 <ibnsifat@gmail.com>
2024-09-07 18:20:39 +0600

    navbar layouyt done

--- src/components/Navbar/Navbar..jsx
+++ src/components/Navbar/Navbar..jsx
@@ -1,3 +1,5 @@
+import { MdMenu } from 'react-icons/md';
+import { SlEarphones } from 'react-icons/sl';
 
 const Navbar = () => {
 const menu=[
@@ -22,25 +24,37 @@ const menu=[
         id:4
     }
 ]
-return <nav>
+return <>
+<div className="bg-brandDark text-white py-6 font-varela">
+<nav className='container flex justify-between items-center'>
     {/* logo section */}
-    <sction>
-        <a href="#">
-            Playing / <span>Market</span>
-        </a>
-    </sction>
-    {/* menu section */}
     <section>
-        <ul>
+        <a href="#" className='text-xl font-bold uppercase'>
+            Playing / <span className='font-extralight  text-white/70'>Market</span>
+        </a>
+    </section>
+    {/* menu section */}
+    <section className='hidden md:block'>
+        <ul className='flex items-center gap-8 '>
             {menu.map((item)=>(
                 <li key={item.id}>
-                    <a href={item.link}>{item.title}</a>
+                    <a href={item.link} className='inline-block text-sm px-3 py-2 uppercase'>{item.title}</a>
                 </li>
             ))}
+            <button className='text-xl ps-14'>
+            <SlEarphones/>
+            </button>
         </ul>
     </section>
     {/* hamburger section */}
-  </nav>;
+    <div >
+    <section className="md:hidden">
+        <MdMenu className='text-4xl'/>
+    </section>
+    </div>
+  </nav>
+  </div>
+</>
 };
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#aa6d6b",
+        brandDark: "#151616",
+        secondary: "#f1c40f",
+        danger: "#e74c3c",
+      },
+      fontFamily: {
+        poppins:["Poppins","sans-serif"],
+        varela:["Varela Round","sans-serif"],
+      },
+      container: {
+        center: true,
+        padding: {
+          Default: "1rem",
+          sm: "2rem",
+          md: "3rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
   },
   plugins: [],
 }
