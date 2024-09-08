55eedceafc05680f716c88ac2153f06c13dc96fa
sifat18 <ibnsifat@gmail.com>
2024-09-09 01:52:05 +0600

    update navbar with framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { UpdateFollower } from "react-mouse-follower";
 import Navbar from "./components/Navbar/Navbar.";
-
 const App = () => {
   return <main className="overflow-x-hidden">
+    <UpdateFollower
+      className="sample"
+      mouseOptions={{
+        backgroundColor: 'white',
+        zIndex: 9999,
+        followSpeed: 1.2,
+      }}
+    >
     <Navbar/>
+
+    </UpdateFollower>
   </main>;
 };
 
--- src/components/Navbar/Navbar..jsx
+++ src/components/Navbar/Navbar..jsx
@@ -1,5 +1,7 @@
 import { MdMenu } from 'react-icons/md';
 import { SlEarphones } from 'react-icons/sl';
+import { UpdateFollower } from 'react-mouse-follower';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
 const menu=[
@@ -25,8 +27,12 @@ const menu=[
     }
 ]
 return <>
-<div className="bg-brandDark text-white py-6 font-varela">
-<nav className='container flex justify-between items-center'>
+<div className="bg-brandDark text-white py-8 font-varela">
+<motion.nav
+initial={{opacity: 0}}
+animate={{opacity: 1}}
+transition={{duration: 1,delay: 0.2}}
+className='container flex justify-between items-center'>
     {/* logo section */}
     <section>
         <a href="#" className='text-xl font-bold uppercase'>
@@ -38,12 +44,35 @@ return <>
         <ul className='flex items-center gap-8 '>
             {menu.map((item)=>(
                 <li key={item.id}>
-                    <a href={item.link} className='inline-block text-sm px-3 py-2 uppercase'>{item.title}</a>
+                     <UpdateFollower
+      className="sample"
+      mouseOptions={{
+        backgroundColor: 'white',
+        zIndex: 9999,
+        followSpeed: 1.2,
+        scale:5,
+        mixBlendMode: 'difference',
+      }}
+    >
+            <a href={item.link} className='inline-block text-sm px-3 py-2 uppercase'>{item.title}</a>
+    </UpdateFollower>
+        
                 </li>
             ))}
+             <UpdateFollower
+      className="sample"
+      mouseOptions={{
+        backgroundColor: 'white',
+        zIndex: 9999,
+        followSpeed: 1.2,
+        scale:5,
+        mixBlendMode: 'difference',
+      }}
+    >
             <button className='text-xl ps-14'>
             <SlEarphones/>
             </button>
+            </UpdateFollower>
         </ul>
     </section>
     {/* hamburger section */}
@@ -52,7 +81,7 @@ return <>
         <MdMenu className='text-4xl'/>
     </section>
     </div>
-  </nav>
+  </motion.nav>
   </div>
 </>
 };
