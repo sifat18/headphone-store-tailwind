49f8b04d9afb084ea6713419ddc0dfaf8f10d44a
sifat18 <ibnsifat@gmail.com>
2024-09-16 23:33:38 +0600

    update banner section animation and banner text

---
+++ src/components/Banner/BannerText.jsx
@@ -0,0 +1,18 @@
+
+import { UpdateFollower } from 'react-mouse-follower';
+const BannerText = () => {
+  return <section className="py-12 text-center"><div className="container">
+    <div className="bg-gradient-to-t from-primary to-primary/70 text-white  rounded-3xl p-8 hover:scale-105 duration-500 hover:shadow-2xl">
+    <UpdateFollower mouseOptions={{
+        backgroundColor:"black",
+        scale:5,
+        followSpeed:0.5,
+        mixBlendMode:"difference",
+        
+    }}>
+    <p className="font-bold text-4xl max-w-[700px] mx-auto leading-normal">Headphones with Good Quality And Affordable Price</p>
+    </UpdateFollower>
+    </div></div></section>;
+};
+
+export default BannerText;
